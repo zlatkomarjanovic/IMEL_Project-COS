2f7cc1f7db0343eecf815afd3512ca7e04400ac8
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-06-24 23:39:54 +0200

    App and contact
    App and contact

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -3,7 +3,28 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            Contact
+            <div className = "container_contact">
+            <div action="action_page.php">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+                <label for="country">Country</label>
+                 <select id="country" name="country">
+                     <option value="australia">Australia</option>
+                     <option value="canada">Canada</option>
+                     <option value="usa">USA</option>
+                 </select>
+                 <label for="subject">Subject</label>
+                    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+
+                    <input type="submit" value="Submit"></input>
+
+            </div>
+
+
+            
+        </div>
         </div>
     )
 }
