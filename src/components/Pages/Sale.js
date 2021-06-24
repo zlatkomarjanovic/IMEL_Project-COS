e5a759a3c73f992e7a199f577713ac821940b9ce
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-06-24 15:42:10 +0200

    Update
    Update

--- src/components/Pages/Sale.js
+++ src/components/Pages/Sale.js
@@ -13,16 +13,16 @@ const Women = () => {
                 
             </div>
 
-           <div className = "overlapping_container"> 
+           <div className = "class1"> 
                 <img className = "sale_img_1" alt = "black man" src = "https://media.istockphoto.com/photos/beautiful-black-man-picture-id1082488184?k=6&m=1082488184&s=612x612&w=0&h=XfcWsVQsQbmyjxNg4YT4aO_dNXbWAU0MKaO1m2hxYrk=" /> 
                 <p>Men</p>
-                <img className = "sale_img_2" alt = "sandals men" src = "https://images-na.ssl-images-amazon.com/images/I/71usR3C-6DL._SL1500_.jpg" /> 
+                <img className = "sale_img_3" alt = "sandals men" src = "https://images-na.ssl-images-amazon.com/images/I/71usR3C-6DL._SL1500_.jpg" /> 
                 <p> <span>Sale </span> Men </p>
            </div>
 
-           <div>
-            <img src = "https://ae01.alicdn.com/kf/HTB175NPax_rK1RkHFqDq6yJAFXaB/Long-Slim-Blend-Outerwear-2019-Women-Overcoat-Wool-Coat-Autumn-Winter-Jacket-Clothes.jpg_960x960.jpg" /> 
-            <img src = "https://res.cloudinary.com/webstore/image/fetch/w_450,c_limit,c_fit,f_auto,e_trim/https%3A%2F%2Fae01.alicdn.com%2Fkf%2FH4d35a363fd844fa5aeb5a481ce51f1090%2F2020-Autumn-And-Winter-Solid-Windbreaker-Jacket-2020-New-Men-Coat-Fashion-Long-Trench-Coats-Lapel.jpg" /> 
+           <div className = "coats">
+            <img className = " coat_men " src = "https://res.cloudinary.com/webstore/image/fetch/w_450,c_limit,c_fit,f_auto,e_trim/https%3A%2F%2Fae01.alicdn.com%2Fkf%2FH4d35a363fd844fa5aeb5a481ce51f1090%2F2020-Autumn-And-Winter-Solid-Windbreaker-Jacket-2020-New-Men-Coat-Fashion-Long-Trench-Coats-Lapel.jpg" />
+            <img className = "coat_women" src = "https://ae01.alicdn.com/kf/HTB175NPax_rK1RkHFqDq6yJAFXaB/Long-Slim-Blend-Outerwear-2019-Women-Overcoat-Wool-Coat-Autumn-Winter-Jacket-Clothes.jpg_960x960.jpg" /> 
            </div>
         </div>
     )
--- src/components/Pages/Women/Women.js
+++ src/components/Pages/Women/Women.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import WomanItem from './WomanItem'
+
+
+const products = [
+    {id: 1, title: 'First Item', price: '12$', imgurl: 'https://www.coverstory.co.in/media/cms/home/category/work.jpg'}, 
+    {id: 2, title: 'Second Item', price: '12$', imgurl: 'https://cdn.shopify.com/s/files/1/2343/4045/products/womens-patchwork-fashion-designer-a-line-midi-dresses-plus-size-womens-fashion-designer-plus-size-dresses-international-womens-clothes_800x.jpg?v=1623919945'}, 
+    {id: 3, title: 'Third Item', price: '12$', imgurl: 'https://cdn.shopify.com/s/files/1/2466/2147/products/WRW0492-PRP-1.jpg?v=1596633662'}, 
+    {id: 4, title: 'Fourth Item', price: '12$', imgurl: 'https://sc04.alicdn.com/kf/HTB13O8yRXXXXXXQapXXq6xXFXXXS.jpg'}
+    
+]
 
 
 const Women = () => {
     return (
         <div>
-           
-           
+            {
+                products.map((product) =>(
+                    <WomanItem 
+                        id={product.id} 
+                        imgurl = {product.imgurl} 
+                        title = {product.title}
+                        price = {product.price}
+                        /> 
+                ))
+            }
         </div>
     )
 }
