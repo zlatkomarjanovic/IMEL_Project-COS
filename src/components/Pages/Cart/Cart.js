e06cce4c2fb7c253cd880bba485d9d562bf66e7b
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-06-27 03:59:24 +0200

    Cart Items
    Cart Items

--- src/components/Pages/Cart/Cart.js
+++ src/components/Pages/Cart/Cart.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import { useCart } from 'react-use-cart'
-import RenderCartItem from './RenderCartItem'
 
-const Cart = ({price, title, image}) => {
+
+const Cart = () => {
     const { 
+        cartTotal,
         isEmpty, 
         totalUniqueItems,
         items,
-        totalItems,
-        cartTotal, 
+        totalItems, 
         updateItemQuantity, 
         removeItem, 
         emptyCart
@@ -17,22 +17,49 @@ const Cart = ({price, title, image}) => {
     if(isEmpty) return(
         <div>
             <h2>
-                Your cart is damn empty
+                >our Cart is Empty
             </h2>
         </div>
     )
     return (
-        <div>
-            <h5>Cart ({totalUniqueItems}) total Items : ({totalItems})</h5>
-            
+        <div className = "outer_container">
+            <h5>Total Unique Items : ({totalUniqueItems}) <br/> Total Items : ({totalItems})</h5>
+            <button onClick = {() => emptyCart()}> </button>
             {items.map((item, index) => { return(
-              <RenderCartItem items = {items} item = {item} /> 
-              )
+                <Fragment>
+             <div className = "cart_container">
+                <img className = "woman_item_image" src = {item.imgurl}/>
+                
+                <div className = "cart_container_data"> 
+                    <h1>{item.title}</h1>
+                    <h3>$ {item.price}</h3>
+                    <p> Quantity of this item : {item.quantity} </p> 
+                    <button
+                        className = "add_to_cart"
+                        onClick = {() => updateItemQuantity(item.id, item.quantity-1)}
+                    > - </button>
+                    <button
+                        className = "add_to_cart"
+                        onClick = {() => updateItemQuantity(item.id, item.quantity+1)}
+                    > + </button>
+                    <button 
+                        className = "button_red"
+                        onClick = {() => removeItem(item.id)}> Remove Item </button>
+                </div>
+                
+             </div>
+             
+             </Fragment>
+             )
+             
             })}
+            
+            <h2>Total Price : $ {cartTotal}</h2>
             
             
         </div>
     )
+    
 }
 
 export default Cart
